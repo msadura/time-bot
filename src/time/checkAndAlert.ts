ac7057f8dc8d4a5ed46cfe0420d3b4bc33e025aa
Marek Sadura <m.sadura@outlook.com>
2021-11-13 20:39:57 +0100

    Add cooldown time

--- src/time/checkAndAlert.ts
+++ src/time/checkAndAlert.ts
@@ -3,9 +3,16 @@ import { sendMessage } from '@app/discord';
 import getDiscounts from '@app/time/getDiscounts';
 import { Discount } from '@app/time/types';
 
+const COOLDOWN_TIME = 1000 * 60 * 5; //5min
+
 let prevDiscountsToAlert: Discount[] = [];
+let prevAlert: number | null = null;
 
 export default async function checkAndAlert() {
+  if (prevAlert && Date.now() - prevAlert < COOLDOWN_TIME) {
+    return;
+  }
+
   const discounts = await getDiscounts();
   const discountsToAlert: Discount[] = [];
 
@@ -18,6 +25,8 @@ export default async function checkAndAlert() {
   });
 
   if (discountsToAlert.length > 0 && haveDiscountsChanged(discountsToAlert)) {
+    prevAlert = Date.now();
+
     sendMessage(
       '--- 🔥 Time discounts alert 🔥 ---' +
         discountsToAlert.map(d => `\n     ${d.bond} - ${d.discount?.toFixed(2)}%`) +
