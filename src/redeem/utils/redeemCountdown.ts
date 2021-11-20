dcbd2155fbdf1cdf292584e193c6c557fb12e620
Marek Sadura <m.sadura@outlook.com>
2021-11-20 09:55:04 -0500

    Deploy extension

--- src/redeem/utils/redeemCountdown.ts
+++ src/redeem/utils/redeemCountdown.ts
@@ -1,8 +1,6 @@
 import { REDEEM_TRIGGER } from '@app/constants';
 import { getNextRebase } from '@app/redeem/utils/getNextRebase';
-import { getSecondsToNextRebase } from '@app/redeem/utils/getSecondsToNextRebase';
 import { ethers } from 'ethers';
-import { secondsUntilBlock } from './secondsUntilBlock';
 
 const LONG_CHECK = REDEEM_TRIGGER; // 10min;
 const SHORT_CHECK = 1 * 60 * 1000; // 1min;
@@ -18,10 +16,6 @@ export async function redeemCountdown(
     const timeLeft = secondsLeftToRebase(rebaseTime * 1000);
 
     let timeout = LONG_CHECK;
-    if (timeLeft < 0) {
-      rebaseTime = await getNextRebase(provider);
-      currentTriggered = false;
-    }
 
     if (timeLeft < REDEEM_TRIGGER && !currentTriggered) {
       triggerReedemCb();
@@ -31,6 +25,12 @@ export async function redeemCountdown(
     if (timeLeft < LONG_CHECK * 2 && !currentTriggered) {
       console.log('🔥', 'Short check');
       timeout = SHORT_CHECK;
+    }
+
+    if (timeLeft < 0) {
+      console.log('🔥', 'Getting new rebase time');
+      rebaseTime = await getNextRebase(provider);
+      currentTriggered = false;
     }
 
     setTimeout(redeemCheck, timeout);
